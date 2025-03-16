6e31e750b26dfb6544576a489750e5f1b51e5e67
Boris Giovanni <ext_bancasic@sagafalabella.com.pe>
2025-03-16 11:43:31 -0500

    mejora de rendimiento

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ const newFriendInput = document.getElementById("amigo");
 const addFriendButton = document.querySelector(".button-add");
 const listFriends = document.getElementById("listaAmigos");
 const addFriendToScreen = () => {
-    listFriends.innerHTML = "";
-    let newList  = "";
-    for (friend of friends) {
-        newList += `<li>${friend}</li>` 
-    } 
-    listFriends.innerHTML = newList;
-}
+	listFriends.innerHTML = "";
+	const fragment = document.createDocumentFragment(); 
+	for (friend of friends) {
+        const li = document.createElement("li"); 
+        li.textContent = friend;
+		fragment.appendChild(li);
+	}
+	listFriends.appendChild(fragment);
+};
 
 const agregarAmigo = () => {
-    const newFriend = newFriendInput.value;
-    if(newFriend.length === 0) {
-        alert("Por favor, inserte un nombre.");
-        return 
-    } 
-    friends.push(newFriend);
-    newFriendInput.value = "";
-    addFriendToScreen();
-}  
+	const newFriend = newFriendInput.value;
+	if (newFriend.length === 0) {
+		alert("Por favor, inserte un nombre.");
+		return;
+	}
+	friends.push(newFriend);
+	newFriendInput.value = "";
+	addFriendToScreen();
+};
 
-
+const sortearAmigo = () => {};
