1d0d0b6b2dd2bbbbd3d89295d624d1e50499dc15
Boris Giovanni <ext_bancasic@sagafalabella.com.pe>
2025-03-16 11:28:56 -0500

    se implemento una función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const friends = [];
+const newFriendInput = document.getElementById("amigo");
+const addFriendButton = document.querySelector(".button-add");
+const agregarAmigo = () => {
+    const newFriend = newFriendInput.value;
+    if(newFriend.length === 0) {
+        alert("Por favor, inserte un nombre.");
+        return 
+    } 
+    friends.push(newFriend);
+    newFriendInput.value = "";
+}  
